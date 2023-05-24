b6f606cc90dbf6e2aebc8752a75fadbb991e602a
Dinesh Anbazhagan <itzmidinesh@gmail.com>
2023-05-25 01:20:07 +0530

    create article detail view

--- blog/articles/views.py
+++ blog/articles/views.py
@@ -1,8 +1,12 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from .models import Article
 
 # Create your views here.
 class ArticleListView(ListView):
     model = Article
-    template_name='home.html'
+    template_name='articles/home.html'
+    
+class ArticleDetailView(DetailView):
+    model = Article
+    template_name='articles/detail.html'
